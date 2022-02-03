84daab7d11da564fd01742fd1ffce8f6e896af26
Qrikko <qrikko@gmail.com>
2022-02-03 22:15:42 +0100

    updated premake

--- premake5.lua
+++ premake5.lua
@@ -1,22 +1,23 @@
 project "dmt"
-    kind "staticLib"
-    language "C"
-    cdialect "C11"
-    staticruntime "on"
+	kind "staticLib"
+	language "C"
+	cdialect "C11"
+	staticruntime "on"
+	toolset "clang"
 
-    targetdir (engine_root .. "/lib/" .. outputdir)
-    objdir (engine_root .. "/bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir (engine_root .. "/lib/" .. outputdir)
+	objdir (engine_root .. "/bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
-        "src/**.h",
-        "src/**.c",
-    }
+	files {
+		"src/**.h",
+		"src/**.c",
+	}
 
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "on"
+	buildoptions { "-fPIC" }
 
-
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
